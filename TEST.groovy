8621d2aa6bdbdb75289565f5d692de870f74dbe0
Abdualla Ibrahim Al-Gouda <abduallah.ibrahim.gouda@gmail.com>
2022-10-25 17:49:37 +0200

    SS

--- TEST.groovy
+++ TEST.groovy
@@ -1,1 +1,31 @@
-TEST
+pipeline {
+      agent any
+      stages {
+            stage('Init') {
+                  steps {
+                        echo 'Hi, '
+                        echo 'We are Starting the Testing'
+                  }
+            }
+            stage('Build') {
+                  steps {
+                        echo 'Building Sample Maven Project'
+                  }
+            }
+             stage('Smoke Test') {
+                  steps {
+                        echo "Testing"
+                  }
+            }
+            stage('Deploy') {
+                  steps {
+                        echo "Deploying in Staging Area"
+                  }
+            }
+            stage('Deploy Production') {
+                  steps {
+                        echo "Deploying in Production Area"
+                  }
+            }
+      }
+}
