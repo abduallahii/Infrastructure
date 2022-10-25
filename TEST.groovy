75f05b65aabf58174ae716ee8c4d5d7abfe928c7
Abdualla Ibrahim Al-Gouda <abduallah.ibrahim.gouda@gmail.com>
2022-10-25 19:42:20 +0200

    SSS

--- TEST.groovy
+++ TEST.groovy
@@ -24,7 +24,8 @@ pipeline {
             }
             stage('Deploy Production') {
                   steps {
-                        echo "Deploying in Production Area"
+                        echo "Deploying in Production Area ..."
+                        echo "Goodbye .."
                   }
             }
       }
